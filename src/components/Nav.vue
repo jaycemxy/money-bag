e2bccdf37f50a452e12cef05b476e44fdfe6f95f
joyce <593205312@qq.com>
2022-07-13 15:02:33 +0800

    添加路由激活样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
   <nav>
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="money"/>
       记账
     </router-link>
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="bill"/>
       标签
     </router-link>
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="chart"/>
       统计
     </router-link>
@@ -32,6 +32,9 @@ nav {
     justify-content: center;
     align-items: center;
     flex-direction: column;
+  }
+  > .item.selected {
+    color: pink;
   }
 }
 </style>
