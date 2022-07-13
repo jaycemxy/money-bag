6ffacdfacf7aa141678d5dd952712e15c2bc1e4b
joyce <593205312@qq.com>
2022-07-13 10:58:36 +0800

    添加三个初始页面

---
+++ src/views/Labels.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Labels页
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Money.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    money页
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
